b53ef9672c28e1289d261945d63b59d709fc0c47
coloradokim <schlesinger.kim@gmail.com>
2016-05-24 15:27:57 -0600

    after 5000 ms, the way to gather student data appears

--- public/script.js
+++ public/script.js
@@ -1,5 +1,12 @@
 function askQuestion(){
   var question = document.getElementById('question').value;
-    console.log(question);
+  var responseTypes = ['Turn and Talk to your neighbor', 'Write down your answer in your text editor', 'Write your answer on a mini-whiteboard', 'Cold Call!']
     document.getElementById("newQuestion").innerHTML = question;
-};
+    document.getElementById("waitTime").innerHTML = "WAIT FOR IT...";
+
+    var intervalID = window.setInterval(provideWaitTime, 5000);
+    function provideWaitTime() {
+      document.getElementById("gatherStudentData").innerHTML = responseTypes[Math.floor(Math.random() * responseTypes.length)];
+      clearInterval(intervalID)
+    }
+  };
